9d49616b51ccc35dce35f7e28043dffd4ac71dbe
kaisa1kaisa <katnikb@yandex.ru>
2025-10-24 15:54:52 +0300

    Hello world

---
+++ DanshinaAA07_project/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, world!"<< endl;
+	char c;
+	cout << "Enter a character" << endl;
+	cin >> c;
+	cout << "Character: " << c << endl;
+	return 0;
+}
